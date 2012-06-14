c59bbaee8c35dbdcf7f2b2e3751ea1b216fd0769
Chris Holmes <cdholmes@uci.edu>
2012-06-14 14:23:03 -0700

    Extensive cleanup
    Removed rcp_mod.F and all references to it.
    Removed netCDF libraries.
    Renamed rcp2_mod.F -> rcp_mod.F
    Changed all USE RCP2_MOD statements
    
    Signed-off-by: Chris Holmes <cdholmes@uci.edu>

--- Makefile_header.mk
+++ Makefile_header.mk
@@ -219,10 +219,6 @@ FFLAGS += -DESMF_TESTBED_
 INCLUDE += -I$(HDR)
 endif
 
-# CDH netCDF for RCPs
-INCLUDE += -I/sopt/netcdf/netcdf3-intel-serial/include
-LINK    += -L/sopt/netcdf/netcdf3-intel-serial/lib -lnetcdf
-
 CC       =
 F90      = ifort $(FFLAGS) $(INCLUDE)
 LD       = ifort $(FFLAGS)
