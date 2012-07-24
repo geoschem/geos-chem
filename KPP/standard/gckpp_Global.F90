9a435581a31aeca39919d1414222222dabf6ecda
Melissa Payer <mpayer@seas.harvard.edu>
2012-07-24 14:02:27 -0400

    Update files in KPP/standard
    New gckpp_*.F90 files were generated using the final version of
    globchem.dat that will be released with v9-01-03. The previous
    KPP files were compiled using an old globchem.dat that included
    bromine chemistry, but did not include other globchem.dat updates
    made in v9-01-03.
    
    Signed-off-by: Melissa Payer <mpayer@seas.harvard.edu>

--- KPP/standard/gckpp_Global.F90
+++ KPP/standard/gckpp_Global.F90
@@ -13,8 +13,8 @@
 !        R. Sander, Max-Planck Institute for Chemistry, Mainz, Germany
 ! 
 ! File                 : gckpp_Global.f90
-! Time                 : Fri Apr 27 11:48:11 2012
-! Working directory    : /mnt/as/home/m/mpayer/KPP
+! Time                 : Tue Jul 24 13:27:00 2012
+! Working directory    : /home/mpayer/KPP
 ! Equation file        : gckpp.kpp
 ! Output root filename : gckpp
 ! 
@@ -39,7 +39,7 @@ MODULE gckpp_Global
   REAL(kind=dp) :: FIX(NFIX)
 ! VAR, FIX are chunks of array C
 !      EQUIVALENCE( C(1),VAR(1) )
-!      EQUIVALENCE( C(136),FIX(1) )
+!      EQUIVALENCE( C(138),FIX(1) )
 ! RCONST - Rate constants (global)
   REAL(kind=dp) :: RCONST(NREACT)
 ! TIME - Current integration time
