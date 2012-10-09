88dc0d686c84cbe345af5b10d1533b85fcd573e1
Bob Yantosca <yantosca@seas.harvard.edu>
2012-10-09 17:21:14 -0400

    Updated gc_chem_utils.F90 and gc_chunk_mod.F90 for ESMF interface
    Corrected minor issues in gc_chem_utils.F90 and gc_chunk_mod.F90, mostly
    typos and omissions.
    
    Also added ProTeX headers to both modules.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- ESMF/gc_chem_utils.F90
+++ ESMF/gc_chem_utils.F90
@@ -1,51 +1,137 @@
-MODULE GC_CHEM_UTILS
-
+!------------------------------------------------------------------------------
+!     NASA/GSFC, Global Modeling and Assimilation Office, Code 910.1 and      !
+!          Harvard University Atmospheric Chemistry Modeling Group            !
+!------------------------------------------------------------------------------
+!BOP
+!
+! !MODULE: GC_Chem_Utils
+!
+! !DESCRIPTION: Utility module for the ESMF interface to GEOS-Chem.
+!\\
+!\\
+! !INTERFACE:
+!
+MODULE GC_Chem_Utils
+!
+! !USES:
+!
   IMPLICIT NONE
   PRIVATE
-  PUBLIC :: GET_SPC_INDX, GET_PBLLEV
-
+!
+! !PUBLIC MEMBER FUNCTIONS:
+!
+  PUBLIC :: GET_SPC_INDX
+  PUBLIC :: GET_PBLLEV
+!
+! !REVISION HISTORY:
+!  09 Oct 2012 - M. Long     - Initial version
+!  09 Oct 2012 - R. Yantosca - Added ProTeX headers
+!  09 Oct 2012 - R. Yantosca - Use F90 free-format indenting (Emacs F90 mode)
+!EOP
+!------------------------------------------------------------------------------
+!BOC
   CONTAINS
-
-  INTEGER FUNCTION GET_SPC_INDX( SPC_NAME, GC_SPC_IDS, GC_SPC_NAMES )
-    !-----------------------------------------------------------------------
-    !     ... RETURN OVERALL SPECIES INDEX ASSOCIATED WITH SPC_NAME
-    !-----------------------------------------------------------------------
-
-    USE COMODE_LOOP_MOD, ONLY : IGAS
-
-    IMPLICIT NONE
-
-    !-----------------------------------------------------------------------
-    !     ... DUMMY ARGUMENTS
-    !-----------------------------------------------------------------------
-    CHARACTER(LEN=*), INTENT(IN) :: SPC_NAME
-    CHARACTER(LEN=*), INTENT(IN) :: GC_SPC_NAMES(:)
-    INTEGER, INTENT(IN)          :: GC_SPC_IDS(:)
-
-    !-----------------------------------------------------------------------
-    !     ... LOCAL VARIABLES
-    !-----------------------------------------------------------------------
+!EOC
+!------------------------------------------------------------------------------
+!          Harvard University Atmospheric Chemistry Modeling Group            !
+!------------------------------------------------------------------------------
+!BOP
+!
+! !IROUTINE: get_spc_indx
+!
+! !DESCRIPTION: Returns the index of a chemical species given its name.
+!\\
+!\\
+! !INTERFACE:
+!
+  FUNCTION GET_SPC_INDX( SPC_NAME, GC_SPC_IDS, GC_SPC_NAMES ) RESULT( SPC_INDX )
+!
+! !USES:
+!
+    !%%%% NOTE: THIS DOESN'T SEEM TO BE USED HERE %%%%
+    !%%%%       SO MAYBE WE CAN DELETE THIS LATER %%%%
+    !USE COMODE_LOOP_MOD, ONLY : IGAS
+!
+! !INPUT PARAMETERS:
+!
+    CHARACTER(LEN=*), INTENT(IN) :: SPC_NAME          ! Species name
+    CHARACTER(LEN=*), INTENT(IN) :: GC_SPC_NAMES(:)   ! Names of all species
+    INTEGER,          INTENT(IN) :: GC_SPC_IDS(:)     ! ID's  of all species
+!
+! !RETURN VALUE:
+!
+    INTEGER                      :: SPC_INDX          ! Index of this species 
+!
+! !REVISION HISTORY: 
+!  09 Oct 2012 - M. Long     - Initial version
+!  09 Oct 2012 - R. Yantosca - Added ProTeX headers
+!  09 Oct 2012 - R. Yantosca - Declare INDX as integer return value
+!EOP
+!------------------------------------------------------------------------------
+!BOC
+!
+! !LOCAL VARIABLES:
+!
     INTEGER :: M
 
-    GET_SPC_INDX = -1
-    DO M = 1, SIZE(GC_SPC_NAMES)
+    ! Initialize
+    SPC_INDX = -1
+
+    ! Loop over all species names
+    DO M = 1, SIZE( GC_SPC_NAMES )
+
+       ! Return the index of the sought-for species
        IF( TRIM( SPC_NAME ) == TRIM( GC_SPC_NAMES(M) ) ) THEN
-          GET_SPC_INDX = GC_SPC_IDS(M)
+          SPC_INDX = GC_SPC_IDS(M)
           EXIT
-       END IF
-    END DO
+       ENDIF
+
+    ENDDO
 
   END FUNCTION GET_SPC_INDX
-
-  SUBROUTINE get_pbllev!(pblht,zmid,ncol,pbllev)
-!    USE ppgrid
-
-!    integer,  intent(in)   :: ncol ! Columns per chunk.                                                                                    
+!EOC
+!------------------------------------------------------------------------------
+!          Harvard University Atmospheric Chemistry Modeling Group            !
+!------------------------------------------------------------------------------
+!BOP
 !
-!    ! Calculate pbllev_cam across ncol                                                                                                     
-!    real, intent(in)   :: pblht(pcols)
-!    real, intent(in)   :: zmid(pcols, pver)
-!    real, intent(out)  :: pbllev(pcols)
+! !IROUTINE: get_pbllev
+!
+! !DESCRIPTION: Get the level of the PBL
+!\\
+!\\
+! !INTERFACE:
+!
+  SUBROUTINE get_pbllev!(pblht,zmid,ncol,pbllev)
+!
+! !USES:
+!
+    !USE ppgrid
+!
+! !INPUT PARAMETERS:
+!
+    ! Comment these out for now
+    !integer,  intent(in)   :: ncol ! Columns per chunk.
+    !
+    ! Calculate pbllev_cam across ncol
+    !real, intent(in)   :: pblht(pcols)
+    !real, intent(in)   :: zmid(pcols, pver)
+    !real, intent(out)  :: pbllev(pcols)
+
+!
+! !REMARKS:
+!  Comment out until further notice.
+!
+! !REVISION HISTORY: 
+!  09 Oct 2012 - M. Long     - Initial version
+!  09 Oct 2012 - R. Yantosca - Added ProTeX headers
+!EOP
+!------------------------------------------------------------------------------
+!BOC
+!
+! !LOCAL VARIABLES:
+!
+
 !    integer                :: i, z
 !
 !    pbllev = 0
@@ -60,5 +146,6 @@ MODULE GC_CHEM_UTILS
 !       ENDDO
 !    ENDDO
   END SUBROUTINE get_pbllev
+!EOC
 
-end module gc_chem_utils
+END MODULE GC_Chem_Utils
