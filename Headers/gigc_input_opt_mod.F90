d46a977d34b964d51749bed611a6625f0c7ca27b
Melissa Sulprizio <mpayer@seas.harvard.edu>
2015-04-09 14:22:29 -0400

    Removed more obsolete options from the Aerosol Menu
    The NAPEMISS and POAEMISSSCALE variables have been removed from
    READ_AEROSOL_MENU (GeosCore/input_mod.F). These scale factors are now
    provided in the HEMCO_Config.rc file and are read and handled by HEMCO.
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- Headers/gigc_input_opt_mod.F90
+++ Headers/gigc_input_opt_mod.F90
@@ -117,8 +117,6 @@ MODULE GIGC_Input_Opt_Mod
      LOGICAL                     :: LCARB              
      LOGICAL                     :: LSOA               
      LOGICAL                     :: LSVPOA
-     REAL(fp)                    :: NAPEMISS
-     REAL(fp)                    :: POAEMISSSCALE
      LOGICAL                     :: LDUST              
      LOGICAL                     :: LDEAD              
      LOGICAL                     :: LSSALT             
@@ -704,8 +702,9 @@ MODULE GIGC_Input_Opt_Mod
 !  03 Dec 2014 - M. Yannetti - Added PRECISION_MOD
 !  03 Dec 2014 - M. Sulprizio- Add fields for Radiation Menu
 !  16 Dec 2014 - R. Yantosca - Removed JLOP, JLOP_PREV; these are in State_Chm
-!  09 Apr 2015 - M. Sulprizio- Removed field for PSC restart file. We now output
-!                              this data to the HEMCO restart file.
+!  09 Apr 2015 - M. Sulprizio- Removed fields for NAPEMISS, POAEMISSSCALE,
+!                              and PST_RST_FILE. These options are now handled
+!                              by HEMCO.
 !EOP
 !------------------------------------------------------------------------------
 !BOC
@@ -885,8 +884,6 @@ CONTAINS
     Input_Opt%LCARB                  = .FALSE.
     Input_Opt%LSOA                   = .FALSE.
     Input_Opt%LSVPOA                 = .FALSE.
-    Input_Opt%NAPEMISS               = 0e+0_fp
-    Input_Opt%POAEMISSSCALE          = 0e+0_fp
     Input_Opt%LDUST                  = .FALSE.
     Input_Opt%LDEAD                  = .FALSE.
     Input_Opt%LSSALT                 = .FALSE.
