bebb617065a059dbca57e99ca975fdc14ce509a2
Michael Long <mslong@rclogin08.rc.fas.harvard.edu>
2016-03-09 14:32:34 -0500

    1) Moved INIT_FLEXCHEM into chemistry_mod.F, because the initial    species mixing ratios were being set with not-yet interpolated    met fields.
    2) Took out a handful of debugging statements used for FlexChem
    
    MSL

--- GeosCore/diagnostics_mod.F90
+++ GeosCore/diagnostics_mod.F90
@@ -214,15 +214,15 @@ CONTAINS
 !    ENDIF
 
     ! KPP diagnostics
-!    CALL DIAGINIT_KPP_RATES( am_I_Root, Input_Opt, RC )
-!    IF ( RC /= GIGC_SUCCESS ) THEN
-!       CALL ERROR_STOP( 'Error in DIAGINIT_KPP', LOC ) 
-!    ENDIF
+    CALL DIAGINIT_KPP_RATES( am_I_Root, Input_Opt, RC )
+    IF ( RC /= GIGC_SUCCESS ) THEN
+       CALL ERROR_STOP( 'Error in DIAGINIT_KPP', LOC ) 
+    ENDIF
 
-!    CALL DIAGINIT_KPP_SPEC( am_I_Root, Input_Opt, RC )
-!    IF ( RC /= GIGC_SUCCESS ) THEN
-!       CALL ERROR_STOP( 'Error in DIAGINIT_KPP', LOC ) 
-!    ENDIF
+    CALL DIAGINIT_KPP_SPEC( am_I_Root, Input_Opt, RC )
+    IF ( RC /= GIGC_SUCCESS ) THEN
+       CALL ERROR_STOP( 'Error in DIAGINIT_KPP', LOC ) 
+    ENDIF
 
     ! Leave with success
     RC = GIGC_SUCCESS
