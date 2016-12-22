95e0d11ff9faedddc43d2298d2a74696b26274f0
Sebastian Eastham <seastham@fas.harvard.edu>
2016-12-21 23:55:36 -0500

    Modified KPP run script to guarantee recovery
    A bash "trap" function has been added to the KPP run script so
    that the customized integrator and het rate calculator functions
    are retained even if the script (or KPP) fails.
    
    Signed-off-by: Sebastian Eastham <seastham@fas.harvard.edu>

--- KPP/BrCl/runAll.sh
+++ KPP/BrCl/runAll.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Runs KPP while preserving the custom het-rate calculation and KPP integrator
 
+function finish {
+  if [[ -e HETCODE ]]; then
+    mv HETCODE gckpp_HetRates.F90
+  fi
+  if [[ -e INTEGRATOR ]]; then
+    mv INTEGRATOR gckpp_Integrator.F90
+  fi
+}
+trap finish EXIT
+
 mv gckpp_HetRates.F90 HETCODE
 mv gckpp_Integrator.F90 INTEGRATOR
 rm *F90
@@ -8,10 +18,6 @@ rm *o
 kpp gckpp.kpp
 if [[ ! -e gckpp_Rates.f90 ]]; then
   echo "KPP failure! Aborting."
-  mv HETCODE gckpp_HetRates.F90
-  mv INTEGRATOR gckpp_Integrator.F90
   exit 1
 fi
 ./rename_f90.sh
-mv HETCODE gckpp_HetRates.F90
-mv INTEGRATOR gckpp_Integrator.F90
