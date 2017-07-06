30cb2e5d0e6ebc2e082930d29b57328577178f30
Bob Yantosca <yantosca@seas.harvard.edu>
2017-07-06 10:54:44 -0400

    Now only test for advected species > 0 in routine SET_MINIT
    In routine SET_MINIT (in GeosCore/strat_chem_mod.F90) there was an error
    check if the sum of State_Chm%Species was negative.  But because the
    non-advected species are currently negative before chemistry (i.e. have
    a MW of -1), then this was producing spurious error messages.  An error
    message was being printed but the run was not being stopped.
    
    We have therefore made the following changes:
    
    (1) Only check if the sum of advected species (not all species) is
        nonzero.  If so, then print an error message and exit SET_MINIT.
    
    (2) Put an error trap in routine INIT_STRAT_CHEM that will exit the
        routine if SET_MINIT returns failure.
    
    (3) Put an error trap in main.F which wills top the run if the error
        from SET_MINIT propagates all the way up.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- GeosCore/strat_chem_mod.F90
+++ GeosCore/strat_chem_mod.F90
@@ -1348,6 +1348,7 @@ CONTAINS
 !
     ! Strings
     CHARACTER(LEN=16)      :: sname
+    CHARACTER(LEN=255)     :: ErrMsg, ThisLoc
 
     ! Scalars
     INTEGER                :: AS, N, NN, NA, nAdvect
@@ -1359,14 +1360,16 @@ CONTAINS
     ! Pointers
     TYPE(Species), POINTER :: SpcInfo
 
+    !================================================================
+    ! Initialize 
     !=================================================================
-    ! INIT_STRAT_CHEM begins here!
+    RC       = GC_SUCCESS
+    ErrMsg   = ''
+    ThisLoc  = ' -> Init_Strat_Chem (in Headers/strat_chem_mod.F90)'
+
     !=================================================================
-
-    ! Assume success
-    RC                       = GC_SUCCESS
-
     ! Get species ID flags
+    !=================================================================
     id_Br                    = Ind_('Br'     )
     id_Br2                   = Ind_('Br2'    )
     id_BrNO3                 = Ind_('BrNO3'  ) 
@@ -1597,7 +1600,11 @@ CONTAINS
     ! Set MINIT. Ignore in ESMF environment because State_Chm%Species
     ! is not yet filled during initialization. (ckeller, 4/6/16)
     CALL SET_MINIT( am_I_Root, Input_Opt, State_Met, State_Chm, RC )
-    IF ( RC /= GC_SUCCESS ) RETURN
+    IF ( RC /= GC_SUCCESS ) THEN
+       ErrMsg = 'Error encountered in routine "Set_MInit"!'
+       CALL GC_Error( ErrMsg, RC, ThisLoc )
+       RETURN
+    ENDIF
 #endif
 
     ! Array to determine the mean tropopause level over the period
@@ -1666,42 +1673,50 @@ CONTAINS
 !
 ! !LOCAL VARIABLES:
 !
-    INTEGER :: N, NA
-    LOGICAL :: UNITCHANGE
-    CHARACTER(LEN=255)     :: MSG, LOC
+    ! Scalars
+    INTEGER            :: N, NA
+    LOGICAL            :: UNITCHANGE
+
+    ! Strings
+    CHARACTER(LEN=255) :: ErrMsg, ThisLoc
 
     !=================================================================
     ! SET_MINIT begins here!
     !=================================================================
-
-    ! Assume success
-    RC                       = GC_SUCCESS
-
-    ! Set location for error handling
-    LOC = 'Routine SET_MINIT in strat_chem_mod.F90'
+    
+    ! Initialize
+    RC      = GC_SUCCESS
+    ErrMsg  = ''
+    ThisLoc = '-> Set_MInit (in GeosCore/strat_chem_mod.F90)'
 
     ! Assume no unit change is necessary and species are already in kg
     UNITCHANGE = .FALSE.
 
-    ! Safety check that STT is non-zero.
-    IF ( SUM(State_Chm%Species) <= 0.0_fp ) THEN
-       CALL GC_Error( 'Negative species concentrations!', RC, LOC )
+    ! Safety check that the advected species are non-zero.
+    IF ( SUM( State_Chm%Species(:,:,:,1:State_Chm%nAdvect) ) <= 0.0_fp ) THEN
+       ErrMsg = 'One or more advected species have negative concentrations!'
+       CALL GC_Error( ErrMsg, RC, ThisLoc )
+       RETURN
     ENDIF
 
     ! If incoming species units are in mixing ratio, convert species 
     ! from [kg/kg dry air] to [kg] so that initial state of atmosphere 
     ! is in same units as chemistry ([kg]), and then convert back after 
     ! MInit is assigned (ewl, 8/10/15)
-    IF ( TRIM(State_Chm%Spc_Units) .eq. 'kg/kg dry' ) THEN
+    IF ( TRIM( State_Chm%Spc_Units ) .eq. 'kg/kg dry' ) THEN
        UNITCHANGE = .TRUE.
        CALL ConvertSpc_KgKgDry_to_Kg( am_I_Root, State_Met, State_Chm, RC )
        IF ( RC /= GC_SUCCESS ) THEN
-          CALL GC_Error('Unit conversion error!', RC, LOC )
+          ErrMsg = 'Unit conversion error!'
+          CALL GC_Error( ErrMsg, RC, ThisLoc )
+          RETURN
        ENDIF
     ELSE IF ( TRIM(State_Chm%Spc_Units) .ne. 'kg' ) THEN
-          MSG = 'Incorrect species units: ' // TRIM(State_Chm%Spc_Units) &
-               // ' (must be kg or kg/kg dry)'
-          CALL GC_Error( MSG, RC, LOC )
+       ErrMsg = 'Incorrect species units: '   // &
+            TRIM(State_Chm%Spc_Units)    // &
+            ' (must be kg or kg/kg dry)'
+       CALL GC_Error( ErrMsg, RC, ThisLoc )
+       RETURN
     ENDIF
 
     ! Loop over only the advected species
@@ -1719,7 +1734,8 @@ CONTAINS
     IF ( UNITCHANGE ) THEN
        CALL ConvertSpc_Kg_to_KgKgDry( am_I_Root, State_Met, State_Chm, RC )
        IF ( RC /= GC_SUCCESS ) THEN
-          CALL GC_Error('Unit conversion error!', RC, LOC)
+          ErrMsg = 'Unit conversion error!'
+          CALL GC_Error( ErrMsg, RC, ThisLoc )
           RETURN
        ENDIF
     ENDIF
