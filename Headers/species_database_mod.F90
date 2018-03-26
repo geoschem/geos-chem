9eac5e2e1275e8dd0f5727c707a996b155a4d165
Melissa Sulprizio <mpayer@seas.harvard.edu>
2018-03-26 11:55:49 -0400

    Fix Henry's law parameters for HCl
    Xuan Wang wrote:
    
       Heff at pH=7 is set to 2e6 for HCl, much lower than it should be.
       H at pH=4.5 cannot be higher than at pH=7. Yang et al. (2005) is
       a Br paper without HCl Henry number shown. I suggest to change
       the HCl values to Hstar=2.05e13 and K0=7e10, which are calculated
       from Sander 2015.
    
       Also in calc_heff in henry_mod.F (around bottom), we have
    
         IF ( thispKa > 0d0 ) THEN
            HEFF = KH * ( 1d0 + 10d0**( pH - thispKa ) )
    
       It looks more reasonable to change to :
    
         IF ( pH >= 0d0 ) THEN
    
       Since pKa can be negative.
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- Headers/species_database_mod.F90
+++ Headers/species_database_mod.F90
@@ -1812,8 +1812,8 @@ CONTAINS
                               Is_Drydep     = T,                            &
                               Is_Wetdep     = T,                            &
                               DD_F0         = 0.0_fp,                       &
-                              DD_Hstar_old  = 2.05e+6_fp,                   &
-                              Henry_K0      = 7.10e+15_f8,                  &
+                              DD_Hstar_old  = 2.05e+13_fp,                  &
+                              Henry_K0      = 7.00e+10_f8,                  &
                               Henry_CR      = 11000.0_f8,                   &
                               WD_RetFactor  = 1.0_fp,                       &
                               RC            = RC )
