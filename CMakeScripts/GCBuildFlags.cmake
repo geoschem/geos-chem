14abb722cc47b3599f1c3d052aadee501b59b3a1
Liam Bindle <liam.bindle@gmail.com>
2018-12-10 20:39:11 -0400

    new workflow to setup build configuration

--- CMakeScripts/GCBuildFlags.cmake
+++ CMakeScripts/GCBuildFlags.cmake
@@ -6,6 +6,7 @@ dependent, whereas definitions depend on the build configuration (i.e. MET,
 GRID, etc.).
 
 Exported variables:
+    IMPL
     GC_DEFINES
     FC_OPTIONS
 
@@ -33,149 +34,46 @@ set_dynamic_default(RUNDIR "${RUNDIR_PROMPT}"
 )
 dump_log(RUNDIR_LOG)
 
+message(STATUS "Bootstrapping ${RUNDIR}")
 
-
-# Verify RUNDIR
+# Check that RUNDIR exists
 if("${RUNDIR}" STREQUAL "${RUNDIR_PROMPT}")
     message(FATAL_ERROR "You haven't set RUNDIR!")
 elseif(NOT IS_ABSOLUTE "${RUNDIR}")
     set(RUNDIR "${CMAKE_BINARY_DIR}/${RUNDIR}")
 endif()
 
-if(NOT EXISTS "${RUNDIR}/getRunInfo")
-    message(FATAL_ERROR "Invalid RUNDIR. ${RUNDIR}/getRunInfo doesn't exist!")
+if(NOT EXISTS "${RUNDIR}")
+    message(FATAL_ERROR "Invalid RUNDIR. ${RUNDIR} does not exist!")
 endif()
 
-# Inspect run directory
-include(GCInspectRunDir)
-
-
-# MET field
-set_dynamic_option(MET ${RUNDIR_MET}
-    LOG GENERAL_OPTIONS_LOG
-    SELECT_EXACTLY 1
-    OPTIONS "GEOS_FP" "MERRA2"
-)
-set_dynamic_default(GC_DEFINES ${MET})
-
-# Check for nested grid
-set_dynamic_option(NESTED "${RUNDIR_NESTED}"
-    LOG GENERAL_OPTIONS_LOG
-    SELECT_EXACTLY 1
-    OPTIONS "TRUE" "FALSE"
-)
-if(${NESTED})
-    # Which nested grid?
-    set_dynamic_option(REGION "${RUNDIR_REGION}"
-        LOG GENERAL_OPTIONS_LOG
-        SELECT_EXACTLY 1
-        OPTIONS "AS" "CH" "CU" "EU" "NA" 
-    )
-    set_dynamic_default(GC_DEFINES NESTED NESTED_${REGION})
-endif()
-
-# Horizontal grid
-if(${NESTED})
-    if("${MET}" STREQUAL "MERRA2") # Nested w/ MERRA2 
-        set_dynamic_option(GRID "${RUNDIR_GRID}"
-            LOG GENERAL_OPTIONS_LOG
-            SELECT_EXACTLY 1
-            OPTIONS "0.5x0.625"
-        )
-    else() # Nested w/ GEOS_FP
-        set_dynamic_option(GRID "${RUNDIR_GRID}"
-            LOG GENERAL_OPTIONS_LOG
-            SELECT_EXACTLY 1
-            OPTIONS "0.25x0.3125"
-        )
-    endif()
-else() # Not nested
-    set_dynamic_option(GRID "${RUNDIR_GRID}"
-        LOG GENERAL_OPTIONS_LOG
-        SELECT_EXACTLY 1
-        OPTIONS "4x5" "2x2.5"
-    )
-endif()
-string(REPLACE "." "" TEMP "GRID${GRID}")
-set_dynamic_default(GC_DEFINES ${TEMP})
-
-# Chemistry mechanism
-set_dynamic_option(MECH "${RUNDIR_MECH}"
-    LOG GENERAL_OPTIONS_LOG
-    SELECT_EXACTLY 1
-    OPTIONS "Standard" "Tropchem" "SOA_SVPOA"
-)
-
-if(${MECH} STREQUAL "Tropchem")
-    set_dynamic_default(GC_DEFINES GRIDREDUCED)
-endif()
-
-
-# Build RRTMG?
-set_dynamic_option(RRTMG "FALSE"
-    LOG GENERAL_OPTIONS_LOG
-    SELECT_EXACTLY 1
-    OPTIONS "TRUE" "FALSE"
-)
-if(${RRTMG})
-    set_dynamic_default(GC_DEFINES "RRTMG")
-endif()
-
-message(STATUS "General settings:")
-dump_log(GENERAL_OPTIONS_LOG)
-
-
-# Get diagnostics
-set_dynamic_default(DIAG 
-    "BPCH_DIAG" "BPCH_TIMESER" "BPCH_TPBC"
-
-    LOG EXTRA_DEFS_LOG
-)
-set_dynamic_default(GC_DEFINES ${DIAG})
-
-
-# Get extra defines
-set_dynamic_default(EXTRA 
-    "UCX" "USE_REAL8" "USE_TIMERS"
-    
-    LOG EXTRA_DEFS_LOG
-)
-set_dynamic_default(GC_DEFINES ${EXTRA})
-
-message(STATUS "Additional definitions:")
-dump_log(EXTRA_DEFS_LOG)
-
-# Get resulting GC_DEFINES
-string(REPLACE " " ";" GC_DEFINES "${GC_DEFINES}")
-set_dynamic_default(GC_DEFINES LOG RESULTING_DEFINES_LOG)
-
-# Get compiler options
-if("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "Intel")
-    set_dynamic_default(FC_OPTIONS
-        -fPIC -cpp -w -auto -noalign "-convert big_endian" -O2 -vec-report0 
-        "-fp-model source" -openmp -mcmodel=medium -shared-intel -traceback
-        -DLINUX_IFORT
-
-        LOG RESULTING_DEFINES_LOG
-    )
-elseif("${CMAKE_Fortran_COMPILER_ID}" STREQUAL "GNU")
-    set_dynamic_default(FC_OPTIONS
-        -cpp -w -std=legacy -fautomatic -fno-align-commons -fconvert=big-endian -fno-range-check -O3 
-        -funroll-loops -fopenmp -mcmodel=medium -fbacktrace -g 
-        
-        -DLINUX_GFORTRAN
-
-        LOG RESULTING_DEFINES_LOG
-    )
+# Inspect the RUNDIR's name to make a guess at if we're doing GCC or GCHP
+get_filename_component(RUNDIR "${RUNDIR}" ABSOLUTE)  # Remove trailing slash if present
+get_filename_component(RUNDIR_NAME "${RUNDIR}" NAME)
+if("${RUNDIR_NAME}" MATCHES "gchp.*")
+    set(IMPL_GUESS "GCHP")
 else()
-    message(FATAL_ERROR "${CMAKE_Fortran_COMPILER_ID} Fortran compiler is not currently supported!")
+    set(IMPL_GUESS "Classic")
 endif()
 
-message(STATUS "Resulting definitions/options:")
-dump_log(RESULTING_DEFINES_LOG)
+message(">>>>>>>>>>>> ${RUNDIR_NAME}")
 
-# Replace ';' character (delimiting lists) with ' '
-string(REPLACE ";" " " FC_OPTIONS "${FC_OPTIONS}")
+# Select an implementation
+message(STATUS "GEOS-Chem implementation type:")
+set_dynamic_option(IMPL "${IMPL_GUESS}"
+    LOG IMPL_LOG
+    SELECT_EXACTLY 1
+    OPTIONS "Classic" "GCHP"
+)
+dump_log(IMPL_LOG)
+
+# Get the implementations build flags
+if("${IMPL}" STREQUAL "GCHP")
+    list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/GCHP/CMakeScripts)
+    include(GCHPBuildFlags)
+else()
+    include(GCClassicBuildFlags)
+endif()
 
 # Get configuration hash
 set(SAFE_CFG_HASHES
