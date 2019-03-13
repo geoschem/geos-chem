ebfff0991a2f8936bfdb1d2310c389cd3324698a
Ryan Pound <rp819@york.ac.uk>
2019-03-13 13:21:32 +0000

    succesful rebuild using new kpp compile

--- KPP/Tropchem/gckpp_Global.F90
+++ KPP/Tropchem/gckpp_Global.F90
@@ -13,7 +13,7 @@
 !        R. Sander, Max-Planck Institute for Chemistry, Mainz, Germany
 ! 
 ! File                 : gckpp_Global.f90
-! Time                 : Fri Mar  8 14:36:51 2019
+! Time                 : Wed Mar 13 11:37:09 2019
 ! Working directory    : /mnt/lustre/users/rp819/GC/Code.12.1.1/KPP/Tropchem
 ! Equation file        : gckpp.kpp
 ! Output root filename : gckpp
@@ -39,7 +39,7 @@ MODULE gckpp_Global
   REAL(kind=dp) :: FIX(NFIX)
 ! VAR, FIX are chunks of array C
 !      EQUIVALENCE( C(1),VAR(1) )
-!      EQUIVALENCE( C(354),FIX(1) )
+!      EQUIVALENCE( C(494),FIX(1) )
 ! RCONST - Rate constants (global)
   REAL(kind=dp) :: RCONST(NREACT)
 ! TIME - Current integration time
