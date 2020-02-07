37b7c04f20d7e6bb2d9e7d83f7ccf58c085bbd14
Melissa Sulprizio <mpayer@seas.harvard.edu>
2020-02-07 11:35:59 -0500

    Increase Max_AdvectSpc for new isoprene species
    The APM simulation is crashing because the number of species listed in the
    Advected Species Menu of input.geos exceeds Input_Opt%Max_AdvectSpc. This
    value is only used to allocate Input_Opt%AdvectSpc_Name, so we will set it
    to a large number.
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- Headers/input_opt_mod.F90
+++ Headers/input_opt_mod.F90
@@ -517,11 +517,7 @@ CONTAINS
     Input_Opt%Max_BPCH_Diag          = 80  ! Mirror MAX_DIAG in CMN_DIAG_mod.F
 #endif
     Input_Opt%Max_Families           = 250
-#ifdef TOMAS
     Input_Opt%Max_AdvectSpc          = 600
-#else
-    Input_Opt%Max_AdvectSpc          = 300
-#endif
     Input_Opt%Max_PassiveSpc         = 50
 
     !----------------------------------------
