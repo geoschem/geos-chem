fc51a267ca4baf3492e8e20a4ec8c5e50db63421
Bob Yantosca <yantosca@seas.harvard.edu>
2021-02-12 17:15:00 -0500

    Add !$OMP COLLAPSE(2) to vectorize the parallel loop in DO_FLEXCHEM
    This commit implements the update in geoschem/geos-chem/pull/496 by
    Chris Holmes:
    
       Previously only the outer L loop was parallel, so there were
       nLev work blocks.  With collapse ( 2 ), both L and J loops execute
       in parallel, giving nLev * nLat work blocks.
    
       The larger number of work blocks allow more even distribution of
       the work across CPUs, improving runtime.  FlexChem performance
       improvement (median of 3 trials):
         * 12% faster with 20 OpenMP threads
         * 32% faster with 50 OpenMP threads
    
       Collapse ( 3 ) (i.e. parallelizing over all I,J,L loops)
       was 1% faster than collapse ( 2 ) in tests with 50 threads.
       Since benefits will be smaller with fewer threads, collapse ( 2 )
       currently seems to be a good balance between performance and overhead.
    
       Other loops within GEOS-Chem may similarly benefit from collapse
       directives.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- GeosCore/flexchem_mod.F90
+++ GeosCore/flexchem_mod.F90
@@ -589,6 +589,7 @@ CONTAINS
     !$OMP PRIVATE( SpcID,    KppID,    F,       P,         Vloc             )&
     !$OMP PRIVATE( Aout,     Thread,   RC,      S,         LCH4             )&
     !$OMP PRIVATE( OHreact,  PCO_TOT,  PCO_CH4, PCO_NMVOC                   )&
+    !$OMP COLLAPSE( 3                                                       )&
     !$OMP SCHEDULE( DYNAMIC,  1                                             )
     DO L = 1, State_Grid%NZ
     DO J = 1, State_Grid%NY
