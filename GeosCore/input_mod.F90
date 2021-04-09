8bc81592785ccdcc518806607e760dbdd2d9a543
J.D. Haskins <jhaskins10@gmail.com>
2021-04-09 16:33:00 -0400

    Add option for wildcard input in ObsPack output species menu of input.geos to track all advected species

--- GeosCore/input_mod.F90
+++ GeosCore/input_mod.F90
@@ -3962,7 +3962,19 @@ CONTAINS
        CALL GC_Error( ErrMsg, RC, ThisLoc )
        RETURN
     ENDIF
+	
+	! Test if there is an "?ALL" wildcard present as the FIRST substring
+    ! argument to the ObsPack output Species Line. 
+    IF ( N==1 .AND. INDEX( SUBSTRS(1) , '?ALL' ) >  0)  THEN
 
+	! If wildcard for all species is requested then update the 
+        ! list of species to track to be the list of advected species 
+	SUBSTRS= Input_Opt%AdvectSpc_Name
+
+	! And update the number of species to track with ObsPack as the # of advected species.  
+	N =Input_Opt%N_ADVECT	
+    ENDIF 
+	
     ! Populate the ObsPack species name list
     Input_Opt%ObsPack_nSpc = N
     DO S = 1, Input_Opt%ObsPack_nSpc
