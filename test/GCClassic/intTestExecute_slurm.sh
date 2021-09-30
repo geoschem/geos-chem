2cde6dd400d04ea422037bc677ac7caea82abc12
Bob Yantosca <yantosca@seas.harvard.edu>
2021-09-30 15:39:23 -0400

    Increase requested time and memory for GEOS-Chem Classic int tests
    The nested-grid fullchem integration tests now require more than 80GB
    of memory.  We have now requested 90GB of memory and 6 hrs of time
    in test/GCClassic/intTestExecute_slurm.sh.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- test/GCClassic/intTestExecute_slurm.sh
+++ test/GCClassic/intTestExecute_slurm.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -c 24
 #SBATCH -N 1
-#SBATCH -t 0-05:00
+#SBATCH -t 0-06:00
 #SBATCH -p huce_cascade
-#SBATCH --mem=80000
+#SBATCH --mem=90000
 #SBATCH --mail-type=END
 
 #------------------------------------------------------------------------------
