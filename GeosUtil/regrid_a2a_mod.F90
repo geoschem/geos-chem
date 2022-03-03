c2d8a3da2fb043ae689b392c128101e498e8a18e
Bob Yantosca <yantosca@seas.harvard.edu>
2022-03-03 12:28:04 -0500

    Fix numerical precision issues in regrid_a2a_mod.F90
    GeosUtil/hco_regrid_a2a_mod.F90
    - Use the same values of PI an d Re as in physconstants.F90 in the
      geoschem/geos-Chem repo, which use values from NIST 2014
    - Change tiny_r4 from 1.0e-20 to 1.0e-30
    - Now use the following IF conditions:
        (a) IF ( ABS( dlat ) > 0.0d0 ) instead of IF ( dlat /= 0.0d0 )
        (b) IF ( ABS( dlat ) > 0.0e0 ) instead of IF ( dlat /= 0.0   )
        (c) IF ( ABS( dlon ) > 0.0d0 ) instead of IF ( dlon /= 0.0d0 )
        (d) IF ( ABS( dlon ) > 0.0e0 ) instead of IF ( dlon /= 0.0   )
      As this is a more numerically robust algorithm for excluding
      values that are exactly zero.
    
    We will make the corresponding updates in the module
    src/Shared/GeosUtil/regrid_a2a_mod.F90 in the geoschem/HEMCO repo.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- GeosUtil/regrid_a2a_mod.F90
+++ GeosUtil/regrid_a2a_mod.F90
@@ -54,22 +54,36 @@ MODULE Regrid_A2A_Mod
 !------------------------------------------------------------------------------
 !BOC
 !
+! !PRIVATE TYPES:
+!
+
+  !---------------------------------------------------------------------------
+  ! These are now kept locally, to "shadow" variables from other parts of
+  ! GEOS-Chem.  This avoids depending on GEOS-Chem code within the core
+  ! HEMCO modules. (bmy, 7/14/14)
+  !---------------------------------------------------------------------------
+  CHARACTER(LEN=255)    :: NC_DIR        ! Directory w/ netCDF files
+  INTEGER               :: OUTNX         ! # of longitudes (x-dimension) in grid
+  INTEGER               :: OUTNY         ! # of latitudes  (y-dimension) in grid
+  REAL(fp), ALLOCATABLE :: OUTLON (:  )  ! Longitude on output grid
+  REAL(fp), ALLOCATABLE :: OUTSIN (:  )  ! Sines of latitudes on output grid
+  REAL(fp), ALLOCATABLE :: OUTAREA(:,:)  ! Surface areas on output grid
+!
 ! !DEFINED PARAMETERS:
 !
   !---------------------------------------------------------------------------
-  ! These were taken from CMN_GCTM_mod.F90.  This helps us to avoid depending
-  ! on GEOS-Chem modules in the core HEMCO modules.  (bmy, 7/14/14)
-  ! NOTE: CMN_GCTM_mod.F90 is now physconstants.F90 (ewl, 1/8/2016)
+  ! Physical constants taken from the GEOS-Chem "physconstants.F90" module,
+  ! which uses values from NIST 2014. (ewl, bmy, 03 Mar 2022)
   !---------------------------------------------------------------------------
-  REAL(fp), PARAMETER :: PI =   3.14159265358979323e+0_fp   ! Pi
-  REAL(fp), PARAMETER :: Re =   6.375d6                 ! Earth radius [m]
+  REAL(fp), PARAMETER :: PI = 3.14159265358979323_fp        ! Pi
+  REAL(fp), PARAMETER :: Re = 6.3710072e+6_fp               ! Earth radius [m]
 
   !---------------------------------------------------------------------------
   ! Tiny numbers for single and double precision. These are being used for
   ! skipping missing values. miss_r4 and miss_r8 are the default missing values
   ! for single and double precision, respectively. (ckeller, 4/8/2017)
   !---------------------------------------------------------------------------
-  REAL*4, PARAMETER   :: tiny_r4 = 1.0e-20
+  REAL*4, PARAMETER   :: tiny_r4 = 1.0e-30  !1.0e-20
   REAL*4, PARAMETER   :: miss_r4 = 0.0e0
   REAL*8, PARAMETER   :: tiny_r8 = 1.0d-40
   REAL*8, PARAMETER   :: miss_r8 = 0.0d0
@@ -755,7 +769,7 @@ CONTAINS
           endif
 100    continue
 !123    q2(i,j) = qsum / ( sin2(j+1) - sin2(j) )
-123    if ( dlat /= 0.0d0 ) q2(i,j) = qsum / dlat
+123    if ( ABS( dlat ) > 0.0d0 ) q2(i,j) = qsum / dlat
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
@@ -952,7 +966,7 @@ CONTAINS
              endif
           endif
 100    continue
-123    if ( dlat /= 0.0d0 ) q2(i,j) = qsum / dlat
+123    if ( ABS( dlat ) > 0.0d0 ) q2(i,j) = qsum / dlat
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
@@ -1150,7 +1164,7 @@ CONTAINS
              endif
           endif
 100    continue
-123    if ( dlat /= 0.0d0 ) q2(i,j) = qsum / dlat
+123    if ( ABS( dlat ) > 0.0d0 ) q2(i,j) = qsum / dlat
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
@@ -1348,7 +1362,7 @@ CONTAINS
           endif
 100    continue
 !123    q2(i,j) = qsum / ( sin2(j+1) - sin2(j) )
-123    if ( dlat /= 0.0 ) q2(i,j) = qsum / dlat
+123    if ( ABS( dlat ) > 0.0e0 ) q2(i,j) = qsum / dlat
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
@@ -1680,7 +1694,7 @@ CONTAINS
              endif
           endif
 100    continue
-123    if ( dlon /= 0.0d0 ) q2(i,j) = qsum / dlon
+123    if ( ABS( dlon ) > 0.0d0 ) q2(i,j) = qsum / dlon
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
@@ -1692,7 +1706,7 @@ CONTAINS
   END SUBROUTINE xmap_r8r8
 !EOC
 !------------------------------------------------------------------------------
-!                  GEOS-Chem Global Chemical Transport Model                  !
+!                   Harmonized Emissions Component (HEMCO)                    !
 !------------------------------------------------------------------------------
 !BOP
 !
@@ -1976,7 +1990,7 @@ CONTAINS
              endif
           endif
 100    continue
-123    if( dlon > 0.0 ) q2(i,j) = qsum / dlon
+123    if( ABS( dlon ) > 0.0e0 ) q2(i,j) = qsum / dlon
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
@@ -2260,7 +2274,7 @@ CONTAINS
              endif
           endif
 100    continue
-123    if ( dlon /= 0.0d0 ) q2(i,j) = qsum / dlon
+123    if ( ABS( dlon ) > 0.0d0 ) q2(i,j) = qsum / dlon
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
@@ -2544,7 +2558,7 @@ CONTAINS
              endif
           endif
 100    continue
-123    if( dlon /= 0.0 ) q2(i,j) = qsum / dlon
+123    if( ABS( dlon ) > 0.0e0 ) q2(i,j) = qsum / dlon
 555    continue
 1000 continue
      !$OMP END PARALLEL DO
