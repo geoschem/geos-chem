04ea1e01b81748199ea76dc0fab0e3f01e51bc9f
Lizzie Lundgren <elundgren@seas.harvard.edu>
2023-03-24 13:04:40 -0400

    Write all GCHP checkpoint (restart) files directly to Restarts directory
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- run/GCHP/runScriptSamples/operational_examples/mit_svante/gchp.run_SVANTE.sh
+++ run/GCHP/runScriptSamples/operational_examples/mit_svante/gchp.run_SVANTE.sh
@@ -26,14 +26,14 @@ NY=$( grep NY GCHP.rc | awk '{print $2}' )
 coreCount=$(( ${NX} * ${NY} ))
 time mpirun -n ${coreCount} ./gchp >> ${log}
 
-# Rename and move restart file and update restart symlink if new start time ok
+# Rename restart file and update restart symlink if new start time ok
 new_start_str=$(sed 's/ /_/g' cap_restart)
 if [[ "${new_start_str}" = "${start_str}" || "${new_start_str}" = "" ]]; then
    echo "ERROR: cap_restart either did not change or is empty."
    exit 1
 else
     N=$(grep "CS_RES=" setCommonRunSettings.sh | cut -c 8- | xargs )    
-    mv gcchem_internal_checkpoint Restarts/GEOSChem.Restart.${new_start_str:0:13}z.c${N}.nc4
+    mv Restarts/gcchem_internal_checkpoint Restarts/GEOSChem.Restart.${new_start_str:0:13}z.c${N}.nc4
     source setRestartLink.sh
 fi
 
