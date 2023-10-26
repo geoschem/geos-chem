d188e58976863a1103b7c681df92e5b14d7ea6c5
Christoph Keller <christoph.a.keller@nasa.gov>
2023-10-26 14:20:22 -0400

    Added checks for negative concentrations after KPP integration. These can cause mass balance violations. This update adds the option to repeat the integration with HSTART reset to zero and increased error tolerances. Both of these factors reduce the risk of negative concentrations. This update only applies to GEOS

--- Interfaces/GCHP/Chem_GridCompMod.F90
+++ Interfaces/GCHP/Chem_GridCompMod.F90
@@ -1339,12 +1339,30 @@ CONTAINS
     ENDIF
 
     ! Turn off three heterogenous reactions in stratosphere
-    CALL ESMF_ConfigGetAttribute( GeosCF, DoIt, Default = 0, &
+    CALL ESMF_ConfigGetAttribute( GeosCF, DoIt, Default = 1, &
                                   Label = "TurnOffHetRates:", __RC__ )
     Input_Opt%TurnOffHetRates = ( DoIt == 1 )
     IF ( Input_Opt%AmIRoot ) THEN
        WRITE(*,*) 'Disable selected het. reactions in stratosphere: ', &
                   Input_Opt%TurnOffHetRates
+    ENDIF
+
+    ! Check for negatives after KPP integration
+    CALL ESMF_ConfigGetAttribute( GeosCF, DoIt, Default = -1, &
+                                  Label = "KppCheckNegatives:", __RC__ )
+    Input_Opt%KppCheckNegatives = DoIt 
+    IF ( Input_Opt%AmIRoot ) THEN
+       WRITE(*,*) 'Check for negative concentrations after KPP integration: ', &
+                  Input_Opt%KppCheckNegatives
+    ENDIF
+
+    ! KPP tolerance inflation factor for second attempt
+    CALL ESMF_ConfigGetAttribute( GeosCF, Val, Default = 1.0, &
+                                  Label = "KppTolScale:", __RC__ )
+    Input_Opt%KppTolScale = Val 
+    IF ( Input_Opt%AmIRoot ) THEN
+       WRITE(*,*) 'Scale KPP tolerances in second integration attempt: ', &
+                  Input_Opt%KppTolScale
     ENDIF
 #endif
 
@@ -2648,7 +2666,6 @@ CONTAINS
 #if defined( MODEL_GEOS )
        ! Set H2O from Q (=SPHU)
        CALL GEOS_SetH2O( GC, Input_Opt, State_Met, State_Chm, State_Grid, Q, __RC__ ) 
-   
 
        ! Set appropriate met fields for lightning
        CALL MetVars_For_Lightning_Run( GC, Import=IMPORT, Export=EXPORT, &
