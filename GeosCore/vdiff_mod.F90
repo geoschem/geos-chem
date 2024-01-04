9db5320437d529e7f30c5590f69769d3b0b39033
Lizzie Lundgren <elundgren@seas.harvard.edu>
2024-01-04 16:14:35 -0500

    Add option to set budget transport trop ht array when calling AirQnt
    Updates in this commit include:
     - Pass State_Diag to AirQnt and all subroutines that call it
     - Add optional input logical Update_Budget_TropHt to AirQnt
     - Update budget trop ht diagnostic array when AirQnt is called within
       Set_H2o_Trac and within GC-Classic main.F
     - Accumulate the budget transport trop ht array since it is updated in
       multiple locations across one timestep
     - Add Budget transport trop ht array to budget transport trop diagnostic
       at the end of each timestep
     - Reset the state_diag budget transport trop ht array to zero at the start
       of each timestep (since accumulated)
    
    NOTE: Do not compute the budget transport trop ht array when AirQnt is
    called within mixing, GC-Classic advection, GC-Classic met-field initial
    read, or GCHP main (gchp_chunk_mod). Species mass changes due to changes
    in tropopause level during mixing and GC-Classic advection are already
    captured within other budget diagnostics. GCHP transport budget diagnostics
    are not used. And initial GC-Classic met-field read would have zero mass
    for initial since species concentrations not yet read (tropopause ht would
    also be zero initially).
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- GeosCore/vdiff_mod.F90
+++ GeosCore/vdiff_mod.F90
@@ -2347,8 +2347,8 @@ CONTAINS
     ! NOTE: Prior to October 2015, air quantities were not updated
     ! with specific humidity modified in VDIFFDR at this point in
     ! the model
-    CALL AirQnt( Input_Opt, State_Chm, State_Grid,                           &
-                 State_Met, RC,        Update_Mixing_Ratio=.TRUE.           )
+    CALL AirQnt( Input_Opt, State_Chm, State_Grid, State_Met, &
+                 State_Diag, RC, Update_Mixing_Ratio=.TRUE. )
 
     ! Trap potential errors
     IF ( RC /= GC_SUCCESS ) THEN
