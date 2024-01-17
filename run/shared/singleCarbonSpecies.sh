98435d78ff57e6e666b3caf2a3d817cc56f5370a
Bob Yantosca <yantosca@seas.harvard.edu>
2024-01-17 15:55:19 -0500

    Update singleCarbonSpecies.sh to remove non-emissions data entries
    run/shared/singleCarbonSpecies.sh
    - If CH4 is excluded, also remove these entries from ExtData.rc:
      - EMIS_SF, OH_SF, CH4_LOSS, GLOBAL_Cl, MANURE_SF, RICE_SF,
        EDGAR_SEASONAL_SF, CONUS_
    - If CO2 is excluded, also remove these entries from ExtData.rc:
      - AVIATION_SURF_CORR
    - If CO is excluded, also remove these entries from ExtData.rc:
      - PCO_CH4, PCO_NMVOC, GMI_LOSS_CO, GMI_PROD_CO, NEI99_DOW_CO, LIQFUEL_
    - If OCS is excluded, also remove these entries from ExtData.rc:
      - StoOCS
    
    This should prevent GCHP errors caused by ExtData.rc entries for other
    species except the one being kept in a single-species carbon simulation.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- run/shared/singleCarbonSpecies.sh
+++ run/shared/singleCarbonSpecies.sh
@@ -253,17 +253,24 @@ function updateExtData() {
         sed -i "/^CAN_/d"               "${file}"
         sed -i "/^GFEI_/d"              "${file}"
         sed -i "/^EDGAR7_CH4_/d"        "${file}"
-        sed -i "/^CAN_/d"               "${file}"
+        sed -i "/CMIP6_CH4_/d"          "${file}"
+        sed -i "/CMIP6_BB_CH4/d"        "${file}"
         sed -i "/^UPDATED_GFED4_CH4/d"  "${file}"
         sed -i "/^JPLW_CH4/d"           "${file}"
         sed -i "/^CH4_/d"               "${file}"
         sed -i "/^\#CH4_/d"             "${file}"
-        sed -i "/CMIP6_CH4_/d"          "${file}"
-        sed -i "/CMIP6_BB_CH4/d"        "${file}"
         sed -i "/RCP3PD_CH4/d"          "${file}"
         sed -i "/RCP45_CH4/d"           "${file}"
         sed -i "/RCP60_CH4/d"           "${file}"
         sed -i "/RCP85_CH4/d"           "${file}"
+	sed -i "/^EMIS_SF/d"            "${file}"
+        sed -i "/^OH_SF/d"              "${file}"
+        sed -i "/^CH4_LOSS/d"           "${file}"
+        sed -i "/^GLOBAL_Cl/d"          "${file}"
+        sed -i "/^MANURE_SF/d"          "${file}"
+        sed -i "/^RICE_SF/d"            "${file}"
+        sed -i "/^EDGAR_SEASONAL_SF/d"  "${file}"
+        sed -i "/^CONUS_/d"             "${file}"
     fi
 
     # If CO2 is in the exclude list, remove CO2 entries.
@@ -280,6 +287,7 @@ function updateExtData() {
         sed -i "/ICOADS_CO2_/d"         "${file}"
         sed -i "/OCEANCO2_/d"           "${file}"
         sed -i "/SIB_BBIO_CO2/d"        "${file}"
+	sed -i "/AVIATION_SURF_CORR/d"  "${file}"
     fi
 
     # If CO is in the exclude list, remove CO entries
@@ -298,12 +306,19 @@ function updateExtData() {
         sed -i "/RCP45_CO/d"            "${file}"
         sed -i "/RCP60_CO/d"            "${file}"
         sed -i "/RCP85_CO/d"            "${file}"
+        sed -i "/PCO_CH4/d"             "${file}"
+        sed -i "/PCO_NMVOC/d"           "${file}"
+        sed -i "/GMI_LOSS_CO/d"         "${file}"
+        sed -i "/GMI_PROD_CO/d"         "${file}"
+        sed -i "/NEI99_DOW_CO/d"        "${file}"
+	sed -i "/LIQFUEL_/d"            "${file}"
     fi
 
     # If OSC is in the exclude list, remove OCS entries
     isItemInList "OCS" "${1}"
     if [[ $? == 0 ]]; then
         sed -i "/^OCS_/d"              "${file}"
+        sed -i "/^StoOCS_/d"           "${file}"
     fi
 }
 
