291210140c46a8c2af2d83760a61d2e84234c80b
Viral Shah <viral.shah@nasa.gov>
2024-04-08 10:15:44 -0400

    Add PM25nit and PM25nh4 diags

--- GeosCore/aerosol_mod.F90
+++ GeosCore/aerosol_mod.F90
@@ -926,7 +926,7 @@ CONTAINS
 
        ! PM2.5 nitrate 
        IF ( State_Diag%Archive_PM25nit ) THEN
-          State_Diag%PM25nit(I,J,L) = ( NIT(I,J,L) * SIA_GROWTH  ) &
+          State_Diag%PM25nit(I,J,L) = ( State_Chm%AerMass%NIT(I,J,L) * SIA_GROWTH  ) &
                                     * ( 1013.25_fp / PMID(I,J,L) ) &
                                     * ( T(I,J,L)   / 298.0_fp    ) &
                                     * 1.0e+9_fp
@@ -934,7 +934,7 @@ CONTAINS
 
        ! PM2.5 ammonium 
        IF ( State_Diag%Archive_PM25nh4 ) THEN
-          State_Diag%PM25nh4(I,J,L) = ( NH4(I,J,L) * SIA_GROWTH  ) &
+          State_Diag%PM25nh4(I,J,L) = ( State_Chm%AerMass%NH4(I,J,L) * SIA_GROWTH  ) &
                                     * ( 1013.25_fp / PMID(I,J,L) ) &
                                     * ( T(I,J,L)   / 298.0_fp    ) &
                                     * 1.0e+9_fp
