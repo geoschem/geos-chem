8d22e7b2709f1c4c45f752adf9180cb6e420d16f
GanLuo <42379787+ganluoasrc@users.noreply.github.com>
2025-06-18 22:23:09 -0400

    Update vdiff_mod.F90
    Using DXSN and DYWE to calculate KINC

--- GeosCore/vdiff_mod.F90
+++ GeosCore/vdiff_mod.F90
@@ -1929,8 +1929,8 @@ CONTAINS
       IF(F_CLD>1.D-4)THEN
         State_Met%KINC(I,J,L) = State_Met%WUP(I,J,L)*&
                  (SQRT(F_CLD)*State_Met%BXHEIGHT(I,J,L)*&
-                 0.5D0*(State_Grid%DYW_M(I,J)+State_Grid%DYE_M(I,J)+&
-                        State_Grid%DXS_M(I,J)+State_Grid%DXN_M(I,J))+&
+                 0.5D0*(State_Grid%DYWE_M(I,J)+State_Grid%DYWE_M(I+1,J)+&
+                        State_Grid%DXSN_M(I,J)+State_Grid%DXSN_M(I,J+1))+&
                   F_CLD*State_Grid%Area_M2(I,J))/&
                  (1.D-30+F_CLD*State_Grid%Area_M2(I,J)*&
                                State_Met%BXHEIGHT(I,J,L))
