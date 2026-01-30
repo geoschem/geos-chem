828d29b6e4ff6951fa09856143ed304f385b763b
Melissa Sulprizio <mpayer@seas.harvard.edu>
2026-01-30 12:12:51 -0500

    Fix typo in GEOS-FP settings file used in GCClassic run directory creation
    In pull request #3131, the GEOS-FP met settings file geosfp.preprocessed_ll.txt
    was renamed to geosfp.nonadv_preprocessed_ll.txt. This was not updated in
    GCClassic/createRunDir.sh so when creating a GEOS-FP run directory for
    GCClassic the met settings were not properly set. For example, in
    geoschem_config.rc it still listed "met_field: ${RUNDIR_MET}." Fixing
    the filename corrects this issue.
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- run/GCClassic/createRunDir.sh
+++ run/GCClassic/createRunDir.sh
@@ -342,7 +342,7 @@ while [ "${valid_met}" -eq 0 ]; do
 	RUNDIR_VARS+="RUNDIR_MET_FIELD_CONFIG='HEMCO_Config.rc.gmao_metfields'\n"
     elif [[ ${met_num} = "2" ]]; then
 	met="geosfp"
-	shared_met_settings=${gcdir}/run/shared/settings/geosfp/geosfp.preprocessed_ll.txt
+	shared_met_settings=${gcdir}/run/shared/settings/geosfp/geosfp.nonadv_preprocessed_ll.txt
 	RUNDIR_VARS+="RUNDIR_MET_FIELD_CONFIG='HEMCO_Config.rc.gmao_metfields'\n"
 
 	# Print warning about GEOS-FP and require user to acknowledge it.
