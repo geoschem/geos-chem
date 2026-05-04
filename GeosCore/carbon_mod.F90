c2220d1a8ed75677ce03ba65ae32ccb0447f6b2d
Betty Croft <bcarlin@dal.ca>
2026-05-04 15:47:42 -0300

    shut off BB3D as default

--- GeosCore/carbon_mod.F90
+++ GeosCore/carbon_mod.F90
@@ -4993,7 +4993,7 @@ CONTAINS
    REAL*4                   :: PRES    ! calculated from State_Met
    REAL(fp)                 :: OC2OM = 1.8d0
    LOGICAL                  :: SGCOAG = .FALSE. ! bc,jrp turn off subgrid coag 18/12/23
-   LOGICAL                  :: BB3D = .TRUE. !naj TOMAS BBPIH 6/26/2024
+   LOGICAL                  :: BB3D = .FALSE. !naj TOMAS BBPIH 6/26/2024
                                   ! set to TRUE and in hcoi_gc_diag_mod.F90 to use 3D GFAS
                                   ! and need to modify HEMCO_Config.rc and ExtData.rc
                                   ! need to eventually move this switch to HEMCO_Config.rc, bc, jrp 26/03/2026
