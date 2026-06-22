7a6d611e5c7efb52df485841177f788091eb9127
Helena <helena_mcdonald@seas.harvard.edu>
2026-06-22 13:51:01 -0400

    Run directory updates to default to C90 resolution.

--- run/GCHP/createRunDir.sh
+++ run/GCHP/createRunDir.sh
@@ -756,11 +756,9 @@ else
 fi
 
 # Set default grid resolution
-if [[ "${met}" == "geosit" && "${adv_flux_src}" == "1hr_mass_flux" ]]; then
-    RUNDIR_VARS+="RUNDIR_CS_RES='30'\n"
-else
-    RUNDIR_VARS+="RUNDIR_CS_RES='24'\n"
-fi
+# Modified as of 14.8.0 to default to C90 in all cases
+RUNDIR_VARS+="RUNDIR_CS_RES='90'\n"
+
 
 # Assign appropriate file paths and settings in HEMCO_Config.rc
 if [[ "${sim_extra_option}" == "benchmark" ]]; then
